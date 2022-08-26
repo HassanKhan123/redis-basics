f5f1606219866707bb07135a1dc6cf9158bb0dda
HassanKhan123 <hk2602809@gmail.com>
2022-08-26 12:49:17 +0500

    added: serialize and deserialize functions

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,14 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		make: 'toyota',
+		color: 'red',
+		model: null || ''
+	});
+
+	const car = await client.hGetAll('car');
+	console.log(car);
+};
 run();
